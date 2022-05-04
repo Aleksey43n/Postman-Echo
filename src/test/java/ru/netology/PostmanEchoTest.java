2b9092701d6cbc22877c64700522bfe3fe0103b8
Alex Norin <aleksey43n@gmail.com>
2022-05-04 20:02:20 +0300

    Test Passed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -22,7 +22,7 @@ public class PostmanEchoTest {
                 .then()
                 .statusCode(200)
                 .contentType(ContentType.JSON)
-                .body("data", equalTo("Data"))
+                .body("data", equalTo("Some Data"))
                 .body("headers.content-length", equalTo("9"))
 
         ;
